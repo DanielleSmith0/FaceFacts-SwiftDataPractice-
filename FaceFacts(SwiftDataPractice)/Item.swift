4ba9c25720df6aebe6c503a28e61631b05c4413d
DanielleSmith0 <Danielahsmith@icloud.com>
2024-04-12 22:39:23 -0600

    Initial Commit

---
+++ FaceFacts(SwiftDataPractice)/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  FaceFacts(SwiftDataPractice)
+//
+//  Created by Danielle Smith on 4/12/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
